d9f9391850e8bcd7b340dbf16b0e5e9e58360737
Denis <shadow.blaz@mail.ru>
2018-02-27 04:18:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "lib.h"
 
 int main (int argc, char *argv[]) {
-  std::cout << "Hello World!";
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "ver. " << version();
   return 0;
 }
